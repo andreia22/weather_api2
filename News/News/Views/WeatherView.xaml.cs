888888ef75a22d70bf6339dce1e64bf1ec7ca1a1
Andréia Pasqual <389487@unoesc.lan>
2023-06-07 21:48:08 -0300

    Segundo commit

---
+++ News/News/Views/WeatherView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using News.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace News.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WeatherView : ContentPage
+    {
+        public WeatherView()
+        {
+            InitializeComponent();
+            Task.Run(async () => await Initialize("Weather"));
+        }
+
+        public WeatherView(string scope)
+        {
+            InitializeComponent();
+            Title = $"{scope} news";
+            Task.Run(async () => await Initialize(scope));
+        }
+
+        private async Task Initialize(string scope)
+        {
+            var viewModel = Resolver.Resolve<WeatherViewModel>();
+            BindingContext = viewModel;
+            await viewModel.Initialize(scope);
+        }
+    }
+}
+    
