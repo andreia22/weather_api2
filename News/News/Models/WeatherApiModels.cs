eb0f3f6dec52ff276d4df4826912498a2f1c6b93
Andréia Pasqual <389487@unoesc.lan>
2023-06-07 22:05:33 -0300

    Terceiro commit

--- News/News/Models/WeatherApiModels.cs
+++ News/News/Models/WeatherApiModels.cs
@@ -6,56 +6,41 @@ namespace News.Models
 {
 
     public class Weather
-
     {
-        public int Id { get; set; }
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
 
-        public string Main { get; set; }
-
-        public string Description { get; set; }
-
-        public string Icon { get; set; }
+    public class Clouds
+    {
+        public int all { get; set; }
     }
 
     public class Main
-
     {
-        public double Temp { get; set; }
-
-        public double Feels_like { get; set; }
-
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
         public double temp_min { get; set; }
-
-        public double Wtemp_max { get; set; }
-
-        public int Pressure { get; set; }
-
-        public double Humidity { get; set; }
-
+        public double temp_max { get; set; }
     }
+
+    public class Root
+    {
+        public int dt { get; set; }
+        public List<Weather> weather { get; set; }
+        public Main main { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+    }
+
 
     public class Wind
     {
-        public double Speed { get; set; }
-
-        public int Deg { get; set; }
-
-        public double Gust { get; }
-    }
-
-    public class Cloud
-    {
-        public int All { get; set; }
-    }
-
-
-    public class WeatherResult
-
-    {
-        public DateTime Dt { get; set; }
-
-        public List<Weather> Weather { get; }
-
-
+        public double speed { get; set; }
+        public int deg { get; set; }
     }
 }
